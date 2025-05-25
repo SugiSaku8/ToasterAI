96984314d94d4775c02b383936a479f1769d6781
Sugisaku8 <sumisj522@gmail.com>
2025-05-25 19:51:10 +0900

    Add detailed JSDoc comments for CONFIG object in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,18 @@
+/**
+ * Gemini API設定オブジェクト。
+ * @typedef {Object} CONFIG
+ * @property {string} API_KEY - Gemini APIのキー。
+ * @property {string} API_URL - Gemini APIのエンドポイントURL。
+ * @property {Object} GENERATION_CONFIG - 生成時のパラメータ設定。
+ * @property {number} GENERATION_CONFIG.temperature - 出力の多様性。
+ * @property {number} GENERATION_CONFIG.topK - サンプリング時の上位K件。
+ * @property {number} GENERATION_CONFIG.topP - サンプリング時の確率しきい値。
+ * @property {number} GENERATION_CONFIG.maxOutputTokens - 最大出力トークン数。
+ * @property {Array<string>} [GENERATION_CONFIG.stopSequences] - 出力停止シーケンス。
+ * @property {Array<Object>} SAFETY_SETTINGS - セーフティ設定。
+ * @property {string} SAFETY_SETTINGS[].category - 有害カテゴリ。
+ * @property {string} SAFETY_SETTINGS[].threshold - ブロック閾値。
+ */
 const CONFIG = {
   API_KEY: "AIzaSyDo7xQq1dIHy1j4xNCmZh2vyzX3rE74PF0",
   API_URL:
